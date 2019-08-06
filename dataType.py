418a154be4d069bf649ed6b7e158ae3b1c6ec178
Alriosa <carlos.alfredo.rios15@gmail.com>
2019-08-06 13:54:52 -0600

    3

---
+++ dataType.py
@@ -0,0 +1,15 @@
+print("Datatypes");
+numericfloat=float(input());
+numericint=int(input());
+string=input();
+
+print(numericfloat)
+print(numericint)
+print(string)
+
+numericint+=1
+print(numericint)
+
+print(type(numericfloat))
+print(type(numericint))
+print(type(string))
