1e02cfec35e11a44681a57514441d1c8b42cbc74
Alriosa <carlos.alfredo.rios15@gmail.com>
2019-08-12 10:59:58 -0600

    4

---
+++ ex1.py
@@ -0,0 +1,15 @@
+#Ex1
+#Alriosa
+
+print("Enter first number");
+number1 = int(input());
+
+print("Enter second number");
+number2 = int(input());
+
+if number1>number2:
+    print("Number one is the major one");
+else:
+    print("Number two is the major one");
+
+exit(); #With this i finalize the code 
---
+++ if.py
@@ -0,0 +1,13 @@
+#If Conditional
+#Alriosa
+
+var1=5
+var2=7
+
+if var1>var2:
+    print("Var1 is major");
+    print(type(var1));
+else:
+    print(type(var2));
+    print("Var2 is major");
+
