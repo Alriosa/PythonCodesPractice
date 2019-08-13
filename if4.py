721f07998796bebe37e1b13e36416ce712d73f65
Alriosa <carlos.alfredo.rios15@gmail.com>
2019-08-13 14:25:59 -0600

    11

--- if4.py
+++ if4.py
@@ -7,9 +7,14 @@ enterprise_earnings = int(input())
 print("number of employee")
 number_employee = int(input())
 
+print("we got gov sup?")
+gov_sup = bool(input())
+
 if enterprise_earnings < 10 and number_employee >10:
     print("The things are going bad")
 elif enterprise_earnings > 10 and number_employee ==10:
     print("Perfect!")
+elif enterprise_earnings <10 and number_employee <10 or gov_sup ==True:
+    print("We got this!")
 else:
     print("Could be better...")
---
+++ upperAndLowercases.py
@@ -0,0 +1,9 @@
+#Upper and lower cases.
+
+print("the first word that im going to write always is going to have the first letter uppercased")
+name= input().capitalize()
+print(name)
+
+print("the words are all going to be uppercased")
+upper = input().upper()
+print(upper)
