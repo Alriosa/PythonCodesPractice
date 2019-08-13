a9939ae466609996fdd4b3e3d6b687e2db1d1e27
Alriosa <carlos.alfredo.rios15@gmail.com>
2019-08-13 15:55:16 -0600

    12

---
+++ cycles.py
@@ -0,0 +1,14 @@
+#For
+
+
+for accumulator in [1,2,3]:
+    print("Howdy Partner!")
+
+
+for i in ["Spring","Summer","Autum","Winter"]: #The number of memories space is the number of time it will do the cycle
+    print("And a one and a two and a three")
+    print(i)
+
+
+for j in ["Pills","Information",3]:
+    print("Hola", end=" ")
