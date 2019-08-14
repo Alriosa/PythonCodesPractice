0aaec24d79dea9f2aacd107b6c5dafcc1ba4c6ba
Alriosa <carlos.alfredo.rios15@gmail.com>
2019-08-14 11:47:42 -0600

    16

--- cycles3.py
+++ cycles3.py
@@ -5,11 +5,9 @@ students_numbers = input("Enter the number of students")
 
 for i in range(int(students_numbers)):
     student = input("Name: ")
-
-array_students.append(int(students_numbers))
+    array_students.append(student)
 
 for i in range(len(array_students)):
-    print()
+    print (array_students[i])
 
 print('Student' , students_numbers)
-
