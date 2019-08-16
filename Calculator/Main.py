c14b9b190984e0f4f3f384c62797bb635079c4a0
Alriosa <carlos.alfredo.rios15@gmail.com>
2019-08-16 11:58:33 -0600

    20

--- Calculator/Main.py
+++ Calculator/Main.py
@@ -1,4 +1,23 @@
 from Calculator.BusinessLayer import MathFunctions
 
-MathFunctions.addition(7,7)
+print("This is the Calculator v1.0")
+number1: int(input("Enter the first number: "))
+number2: int(input("Enter the second number: "))
+
+print("Now , what would you like to do?")
+print("1 - to Addition")
+print("2 - to Substraction")
+print("3 - to Multiply")
+print("4 - to Divide")
+option = int(input())
+
+if(option==1):
+    MathFunctions.addition(number1,number2)
+elif(option==2):
+    MathFunctions.minus(number1,number2)
+elif(option==3):
+    MathFunctions.multiplication(number1,number2)
+elif(option==4):
+    MathFunctions.division(number1,number2)
+
 
